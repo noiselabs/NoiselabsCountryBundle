c848f825befd23150a5063d0c23cffe67306435c
Vítor Brandão <noisebleed@noiselabs.org>
2013-01-24 15:45:20 +0000

    Add CountryManager and service "noiselabs.country.country_manager"

---
+++ CountryManager.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * This file is part of NoiseLabs-CountryBundle
+ *
+ * NoiseLabs-CountryBundle is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * NoiseLabs-CountryBundle is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with NoiseLabs-CountryBundle; if not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2012 Vítor Brandão
+ *
+ * @category    NoiseLabs
+ * @package     CountryBundle
+ * @copyright   (C) 2013 Vítor Brandão <noisebleed@noiselabs.org>
+ * @license     http://www.gnu.org/licenses/lgpl-3.0-standalone.html LGPL-3
+ * @link        http://www.noiselabs.org
+ */
+
+namespace NoiseLabs\Bundle\CountryBundle;
+
+/**
+ * Country Manager.
+ *
+ * @author Vítor Brandão <noisebleed@noiselabs.org>
+ */
+class CountryManager
+{
+    /**
+     * @var string
+     * Path to the directory containing countries data.
+     */
+    protected $dataDir;
+
+    /**
+     * @var array
+     * Cached data.
+     */
+    protected $dataCache = array();
+
+    /**
+     * @var array
+     * Available data sources.
+     */
+    protected $dataSources = array('icu', 'cldr');
+
+    /**
+     * Constructor.
+     *
+     * @param string|null $dataDir Path to the directory containing countries data
+     */
+    public function __construct($dataDir = null)
+    {
+        if (!isset($dataDir)) {
+            $r = new \ReflectionClass('Umpirsky\Country\Builder\Builder');
+            $dataDir = sprintf('%s/../../../../country', dirname($r->getFileName()));
+        }
+
+        if (!is_dir($dataDir)) {
+            throw new \RuntimeException(sprintf('Unable to locate the country data directory at "%s"', $dataDir));
+        }
+
+        $this->dataDir = realpath($dataDir);
+    }
+
+    /**
+     * @return string The country data directory.
+     */
+    public function getDataDir()
+    {
+        return $this->dataDir;
+    }
+
+    /**
+     * @param string $locale The locale
+     * @param string $source Data source: "icu" or "cldr"
+     *
+     * @return array
+     */
+    public function getList($locale = 'en', $source = 'cldr')
+    {
+        return $this->loadData($locale, strtolower($source));
+    }
+
+    /**
+     * @param string $locale The locale
+     * @param string $source Data source.
+     * @param array  $data   An array (list) with country data
+     */
+    public function setList($locale, $source, array $data)
+    {
+        $this->dataCache[$locale][strtolower($source)] = $data;
+
+        return $this;
+    }
+
+    /**
+     * A lazy-loader that loads data from a PHP file if it is not stored in
+     * memory yet.
+     *
+     * @param string $locale The locale
+     * @param string $source Data source.
+     *
+     * @return array An array (list) with country
+     */
+    protected function loadData($locale, $source)
+    {
+        if (!isset($this->dataCache[$locale][$source])) {
+            if (!in_array($source, $this->dataSources)) {
+                throw new \InvalidArgumentException(sprintf('Unknown data source "%s". The available ones are: "%s"',
+                $source, implode('", "', $this->dataSources)));
+            }
+
+            $file = sprintf('%s/%s/%s/country.php', $this->dataDir, $source, $locale);
+            if (!is_file($file)) {
+                throw new \RuntimeException(sprintf('Unable to load the country data file "%s"', $file));
+            }
+
+            $this->dataCache[$locale][$source] = require_once $file;
+        }
+
+        return $this->dataCache[$locale][$source];
+    }
+}
--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('noise_labs_country');
+        $rootNode = $treeBuilder->root('noiselabs_country');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
